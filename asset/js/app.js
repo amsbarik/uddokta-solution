eb853a28e685c4330099b292d180b5e497ddb9c4
amsbarik <amsbarik24@gmail.com>
2024-09-22 10:05:45 +0600

    services, portfolio, clients section design done!

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,4 +1,4 @@
-
+// home page js start  ///////////////////////////////
 // navbar scroll js start 
 document.addEventListener('DOMContentLoaded', function () {
 const navbar = document.querySelector('.navbar');
@@ -37,36 +37,20 @@ document.addEventListener('DOMContentLoaded', function () {
   
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////// home page
-// product slider js start 
+// portfolio slider js start 
 document.querySelectorAll('.slider-wrapper').forEach((sliderWrapper, index) => {
-    const productCards = sliderWrapper.querySelectorAll('.product-card');
+    const productCards = sliderWrapper.querySelectorAll('.portfolio-card');
     const prevBtn = sliderWrapper.closest('.slider-container').querySelector('.prev-btn');
     const nextBtn = sliderWrapper.closest('.slider-container').querySelector('.next-btn');
     let currentIndex = 0;
     let slideInterval;
 
     function slideTo(index) {
-        const cardWidth = productCards[0].offsetWidth + 24; // Adjust according to product-card css margin 8*2=16 (margin: 0 8px;)
+        const cardWidth = productCards[0].offsetWidth + 0; // Adjust according to portfolio-card css margin 8*2=16 (margin: 0 8px;)
         const translateXValue = -(index * cardWidth);
         sliderWrapper.style.transform = `translateX(${translateXValue}px)`;
 
-        if (index < productCards.length - 5) {
+        if (index < productCards.length - 4) {
             sliderWrapper.style.transform += `translateX(-${cardWidth * 0.4}px)`;
         }
 
@@ -74,7 +58,7 @@ document.querySelectorAll('.slider-wrapper').forEach((sliderWrapper, index) => {
     }
 
     function nextSlide() {
-        if (currentIndex < productCards.length - 5) {
+        if (currentIndex < productCards.length - 4) {
             slideTo(currentIndex + 1);
         } else {
             slideTo(0);
@@ -85,7 +69,7 @@ document.querySelectorAll('.slider-wrapper').forEach((sliderWrapper, index) => {
         if (currentIndex > 0) {
             slideTo(currentIndex - 1);
         } else {
-            slideTo(productCards.length - 5);
+            slideTo(productCards.length - 4);
         }
     }
 
@@ -110,59 +94,20 @@ document.querySelectorAll('.slider-wrapper').forEach((sliderWrapper, index) => {
 
 
 
-// panjabi 3 items slide js start
-document.querySelectorAll('.slider-3items-wrapper').forEach((sliderWrapper, index) => {
-    const productCards = sliderWrapper.querySelectorAll('.product-card');
-    const prevBtn = sliderWrapper.closest('.slider-3items-container').querySelector('.prev-btn');
-    const nextBtn = sliderWrapper.closest('.slider-3items-container').querySelector('.next-btn');
-    let currentIndex = 0;
-    let slideInterval;
 
-    function slideTo(index) {
-        const cardWidth = productCards[0].offsetWidth + 24; // Adjust according to product-card css margin 8*2=16 (margin: 0 8px;)
-        const translateXValue = -(index * cardWidth);
-        sliderWrapper.style.transform = `translateX(${translateXValue}px)`;
 
-        if (index < productCards.length - 3) {
-            sliderWrapper.style.transform += `translateX(-${cardWidth * 0.4}px)`;
-        }
 
-        currentIndex = index;
-    }
 
-    function nextSlide() {
-        if (currentIndex < productCards.length - 3) {
-            slideTo(currentIndex + 1);
-        } else {
-            slideTo(0);
-        }
-    }
 
-    function prevSlide() {
-        if (currentIndex > 0) {
-            slideTo(currentIndex - 1);
-        } else {
-            slideTo(productCards.length - 3);
-        }
-    }
 
-    nextBtn.addEventListener('click', nextSlide);
-    prevBtn.addEventListener('click', prevSlide);
 
-    function startAutoSlide() {
-        slideInterval = setInterval(nextSlide, 3000); // Adjust interval as needed
-    }
 
-    function stopAutoSlide() {
-        clearInterval(slideInterval);
-    }
 
-    sliderWrapper.addEventListener('mouseover', stopAutoSlide);
-    sliderWrapper.addEventListener('mouseout', startAutoSlide);
 
-    startAutoSlide();
-    slideTo(0); // Start with the first card showing
-});
+
+
+///////////////////////////////// home page  before 
+
 
 
 // testimonial-sliders js start
@@ -174,7 +119,7 @@ document.querySelectorAll('.testimonial-slider-wrapper').forEach((sliderWrapper,
     let slideInterval;
 
     function slideTo(index) {
-        const cardWidth = productCards[0].offsetWidth + 24; // Adjust according to product-card css margin 8*2=16 (margin: 0 8px;)
+        const cardWidth = productCards[0].offsetWidth + 24; // Adjust according to portfolio-card css margin 8*2=16 (margin: 0 8px;)
         const translateXValue = -(index * cardWidth);
         sliderWrapper.style.transform = `translateX(${translateXValue}px)`;
 
